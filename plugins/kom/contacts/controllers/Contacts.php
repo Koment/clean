d9ba48c4f14852a1fc5b0622e0c339a75dde224f
Koment <1@1.ru>
2020-05-19 21:23:03 +0300

    change edit to preview

--- plugins/kom/contacts/controllers/Contacts.php
+++ plugins/kom/contacts/controllers/Contacts.php
@@ -3,10 +3,13 @@
 use Backend\Classes\Controller;
 use BackendMenu;
 
+use Redirect;
+use Backend;
+
 class Contacts extends Controller
 {
     public $implement = [        'Backend\Behaviors\ListController',        'Backend\Behaviors\FormController',        'Backend\Behaviors\ReorderController'    ];
-    
+
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
     public $reorderConfig = 'config_reorder.yaml';
@@ -16,4 +19,26 @@ class Contacts extends Controller
         parent::__construct();
         BackendMenu::setContext('Kom.Contacts', 'main-menu-item');
     }
+
+    // public function create(){
+    //
+    //   // return Redirect::back();
+    // }
+    //
+    // public function update(){
+    //
+    //   // return Redirect::back();
+    //
+    //   // return Redirect::to($this->makeRedirect('preview'));
+    //   //
+    //   // $id = $this->model->id;
+    //   //
+    //   // return Backend::redirect('preview/');
+    //   //
+    //   // $model = $this->config->modelClass;
+    //   //
+    //   // return $this->makeRedirect('preview', $this->model);
+    // }
+
+    
 }
